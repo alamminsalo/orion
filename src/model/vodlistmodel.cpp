dc627a0ce3be0015838c1c859e9545be8de0b872
antti l <antti.lamminsalo@gmail.com>
2016-10-11 16:00:58 +0300

    Added licence headers

--- src/model/vodlistmodel.cpp
+++ src/model/vodlistmodel.cpp
@@ -1,4 +1,18 @@
-﻿#include "vodlistmodel.h"
+﻿/*
+ * Copyright © 2015-2016 Antti Lamminsalo
+ *
+ * This file is part of Orion.
+ *
+ * Orion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Orion.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "vodlistmodel.h"
 
 VodListModel::VodListModel()
 {
--- src/util/jsonparser.cpp
+++ src/util/jsonparser.cpp
@@ -1,3 +1,17 @@
+/*
+ * Copyright © 2015-2016 Antti Lamminsalo
+ *
+ * This file is part of Orion.
+ *
+ * Orion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Orion.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "jsonparser.h"
 #include <QUrl>
 
