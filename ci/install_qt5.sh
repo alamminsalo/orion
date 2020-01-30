c3a410508a88406df3ded65c1fbd6d0d4c54f7f2
alamminsalo <antti.lamminsalo@bitcomp.fi>
2020-01-30 14:28:55 +0200

    Revert "use plain wget"
    This reverts commit 3d9a14e77561b6e34d5e258ed369ad83ef4ec846.

--- ci/install_qt5.sh
+++ ci/install_qt5.sh
@@ -10,14 +10,14 @@ cd $HOME/Qt
 export QT_VERSION=$1
 ENVFILE=qt-${QT_VERSION}.env
 
-sudo apt -y install libssl-dev libssl1.0.0
+sudo apt -y install axel libssl-dev libssl1.0.0 > /dev/null
 
 if [[ ! -f ./${ENVFILE} ]]; then
     wget -q https://raw.githubusercontent.com/mrgreywater/qtci/master/bin/extract-qt-installer
     chmod +x extract-qt-installer
 
     INSTALLER=qt-opensource-linux-x64-${QT_VERSION}.run
-    wget https://download.qt.io/archive/qt/$(echo ${QT_VERSION} |cut -d "." -f-2)/${QT_VERSION}/${INSTALLER}
+    axel --quiet -n 16 https://download.qt.io/archive/qt/$(echo ${QT_VERSION} |cut -d "." -f-2)/${QT_VERSION}/${INSTALLER}
 
     ./extract-qt-installer --list-packages $PWD/$INSTALLER
     ./extract-qt-installer $PWD/$INSTALLER $PWD
