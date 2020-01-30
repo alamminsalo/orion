d91c94265ddc4a5fe2317f8ee2d2086449366a36
alamminsalo <antti.lamminsalo@bitcomp.fi>
2020-01-30 13:41:01 +0200

    use brew link

--- ci/prepare_osx.sh
+++ ci/prepare_osx.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 cat << EOM > ci/osx.env
-export QTDIR=$(brew info --json=v1 qt | jq -r '.[0].bottle.stable.cellar + "/" + .[0].name + "/" + .[0].installed[0].version')
-export PATH=$QTDIR/bin:$PATH
 EOM
